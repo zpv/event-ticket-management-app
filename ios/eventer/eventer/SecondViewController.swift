f177ddfd9ebacdcc26e59ea43dd8c2ab8419dbaf
Eric Mikulin <ericm99@gmail.com>
2017-03-18 14:05:49 -0700

    Initial IOS project creation

---
+++ ios/eventer/eventer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  eventer
+//
+//  Created by Eric Mikulin on 2017-03-18.
+//  Copyright © 2017 Eric Mikulin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
