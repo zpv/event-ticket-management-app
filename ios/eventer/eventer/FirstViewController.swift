317f8e4180bf306fac741445b23b1435bf439c48
Eric Mikulin <ericm99@gmail.com>
2017-03-18 16:22:06 -0700

    Almost have QR code shit working
    Following tutorial, doesn't seem to work yet, investigating

--- ios/eventer/eventer/FirstViewController.swift
+++ ios/eventer/eventer/FirstViewController.swift
@@ -7,12 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+    @nonobjc func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds;
+        }
+        
+        let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+        qrCodeFrameView?.frame = barCodeObject.bounds
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+        // as the media type parameter.
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        var input: AnyObject! = nil
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice)
+        } catch {
+            // If any error occurs, simply log the description of it and don't continue any more.
+            return
+        }
+        
+        // Initialize the captureSession object.
+        captureSession = AVCaptureSession()
+        // Set the input device on the capture session.
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        // Set delegate and use the default dispatch queue to execute the call back
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+    
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Start video capture.
+        captureSession?.startRunning()
+        
+        
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.green.cgColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubview(toFront: qrCodeFrameView!)
     }
 
     override func didReceiveMemoryWarning() {
