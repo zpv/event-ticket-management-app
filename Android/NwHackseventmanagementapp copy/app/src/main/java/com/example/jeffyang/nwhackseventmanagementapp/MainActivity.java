40103ba0d1804a5a4ab057b2e33d23038f1a719e
Eric Mikulin <ericm99@gmail.com>
2017-03-18 22:53:27 -0700

    Android Attempts

--- Android/NwHackseventmanagementapp copy/app/src/main/java/com/example/jeffyang/nwhackseventmanagementapp/MainActivity.java
+++ Android/NwHackseventmanagementapp copy/app/src/main/java/com/example/jeffyang/nwhackseventmanagementapp/MainActivity.java
@@ -1,129 +1,63 @@
 package com.example.jeffyang.nwhackseventmanagementapp;
 
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.content.Intent;
-import android.view.View;
 
-import android.widget.TextView;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
+public class MainActivity extends AppCompatActivity {
+    private static final int ZXING_CAMERA_PERMISSION = 1;
+    private Class<?> mClss;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_main);
+        setupToolbar();
+    }
 
+    public void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
+    public void launchSimpleActivity(View v) {
+        launchActivity(SimpleScannerActivity.class);
+    }
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, ZXingScannerView.ResultHandler {
-        private ZXingScannerView mScannerView;
-        Button scancode,generatecode;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            scancode=(Button)findViewById(R.id.button2);
-            generatecode=(Button)findViewById(R.id.button);
-
-            mScannerView = new ZXingScannerView(MainActivity.this);
-            generatecode.setOnClickListener(this);
-            scancode.setOnClickListener(this);
+    public void launchActivity(Class<?> clss) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            mClss = clss;
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, ZXING_CAMERA_PERMISSION);
+        } else {
+            Intent intent = new Intent(this, clss);
+            startActivity(intent);
         }
-
-        @Override
-        public void onClick(View v) {
-
-            if (v==generatecode)
-            {
-                Intent i=new Intent(MainActivity.this,QRCodeActivity.class);
-                startActivity(i);
-            }
-            if(v==scancode)
-            {
-                ScanCode();
-            }
-        }
-
-    private void ScanCode() {
-
-        mScannerView = new ZXingScannerView(this);
-
-
-        // Programmatically initialize the scanner view
-
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this);
-
-// Register ourselves as a handler for scan results.
-
-        mScannerView.startCamera();
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mScannerView.stopCamera();
-    }
-        @Override
-        public void handleResult(Result result) {
-            // Do something with the result here
-
-            Toast.makeText(MainActivity.this,"This is your Text"+result.getText(),Toast.LENGTH_SHORT);
-            Log.e("handler", result.getText());
-
-// Prints scan results
-
-            Log.e("handler", result.getBarcodeFormat().toString());
-
-// Prints the scan format (qrcode)
-
-        }
-
-
-        /* broken gonna try somehting else
-        public void QrScanner(View view){
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view<br />
-            setContentView(mScannerView);
-                    mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.<br />
-            mScannerView.startCamera();         // Start camera<br />
-        }
-    @Override
-        public void onPause() {
-                super.onPause();
-                mScannerView.stopCamera();   // Stop camera on pause<br />
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case ZXING_CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(mClss != null) {
+                        Intent intent = new Intent(this, mClss);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(this, "Please grant camera permission to use the QR Scanner", Toast.LENGTH_SHORT).show();
+                }
+                return;
         }
     }
-
-    private ZXingScannerView mScannerView;
-    //@Override
-
-
-    public void handleResult(Result rawResult) {
-        // Do something with the result here</p>
-        Log.e("handler", rawResult.getText()); // Prints scan results<br />
-        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)</p>
-        // show the scanner result into dialog box.<br />
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setMessage(rawResult.getText());
-        AlertDialog alert1 = builder.create();
-        alert1.show();
-        // If you would like to resume scanning, call this method below:<br />
-        // mScannerView.resumeCameraPreview(this);<br />
-    }
-*/
-//instructables!!!!!!
-    private void goToSecondActivity() {
-        Intent intent = new Intent(this, Event1Activity.class);
-        startActivity(intent);
-    }
-
-
 }
-
 
